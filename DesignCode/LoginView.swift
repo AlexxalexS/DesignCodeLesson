ba8ac4a9ff0eb791d12507517d930fabb2c0a62a
Alex <alxsar98@gmail.com>
2021-06-07 14:13:44 +0300

    feat: inputs email and password

--- DesignCode/LoginView.swift
+++ DesignCode/LoginView.swift
@@ -8,9 +8,8 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State var show = false
-    @State var viewState = CGSize.zero
-    @State var isDragging = false
+    @State var email = ""
+    @State var password = ""
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -20,68 +19,55 @@ struct LoginView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .ignoresSafeArea(edges: .bottom )
             
+            CoverView()
+            
             VStack {
-                GeometryReader { geometry in
-                    Text("Larn design & code.\nFrom scratch.")
-                        .font(.system(size: geometry.size.width/10, weight: .bold))
-                        .foregroundColor(.white )
-                        .position(x: geometry.size.width/2, y: geometry.size.height/2)
-                }
-                .frame(maxWidth: 375, maxHeight: 100)
-                .padding(.horizontal, 16)
-                .offset(x: viewState.width/17, y: viewState.height/17)
-                
-                Text("80 hours of courses for SwiftUI, React and design tools.")
-                    .font(.subheadline)
-                    .frame(width: 250)
-                    .offset(x: viewState.width/20, y: viewState.height/20)
+                HStack {
+                    Image(systemName: "person.crop.circle.fill")
+                        .foregroundColor(Color(#colorLiteral(red: 0.6549019608, green: 0.7137254902, blue: 0.862745098, alpha: 1)))
+                        .frame(width: 44, height: 44)
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                        .padding(.leading)
                     
+                    
+                    TextField("Your Email".uppercased(), text: $email)
+                        .keyboardType(.emailAddress)
+                        .font(.subheadline)
+                        //                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.leading)
+                        .frame(height: 44)
+                }
                 
-                Spacer()
+                Divider().padding(.leading, 80)
+                
+                HStack {
+                    Image(systemName: "lock.fill")
+                        .foregroundColor(Color(#colorLiteral(red: 0.6549019608, green: 0.7137254902, blue: 0.862745098, alpha: 1)))
+                        .frame(width: 44, height: 44)
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                        .padding(.leading)
+                    
+                    
+                    SecureField("Password".uppercased(), text: $password)
+                        .keyboardType(.emailAddress)
+                        .font(.subheadline)
+                        //                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.leading)
+                        .frame(height: 44)
+                }
+                
             }
-            .multilineTextAlignment(.center)
-            .padding(.top, 100)
-            .frame(height: 477)
+            .frame(height: 136)
             .frame(maxWidth: .infinity)
-            .background(
-                ZStack {
-                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
-                        .offset(x: -150, y: -200)
-                        .rotationEffect(Angle(degrees: show ? 360 + 90 : 90))
-                        .blendMode(.plusDarker)
-                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
-                        .onAppear { self.show = true }
-                    
-                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
-                        .offset(x: -200, y: -250)
-                        .rotationEffect(Angle(degrees: show ? 360 : 0 ), anchor: .leading)
-                        .blendMode(.overlay)
-                        .animation(Animation.linear(duration: 100).repeatForever(autoreverses: false))
-                }
-            )
-            .background(
-                Image(uiImage: #imageLiteral(resourceName: "Card3"))
-                    .offset(x: viewState.width/25, y: viewState.height/25)
-                , alignment: .bottom)
-            .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
+            .background(BlurView(style: .systemMaterial))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .scaleEffect(isDragging ? 0.9 : 1)
-            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
-            .rotation3DEffect(
-                Angle(degrees: 5),
-                axis: (x: viewState.width, y: viewState.height, z: 0)
-            )
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        self.viewState = value.translation
-                        self.isDragging = true
-                    }
-                    .onEnded { value in
-                        self.viewState = .zero
-                        self.isDragging = false
-                    }
-            )
+            .shadow(color: Color.black.opacity(0.15), radius: 20, x: 0, y: 20)
+            .padding(.horizontal)
+            .offset(y: 460)
         }
     }
 }
@@ -89,6 +75,79 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
-            //.previewDevice("iPad Air 2")
+        //.previewDevice("iPad Air 2")
+    }
+}
+
+struct CoverView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    
+    var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                Text("Larn design & code.\nFrom scratch.")
+                    .font(.system(size: geometry.size.width/10, weight: .bold))
+                    .foregroundColor(.white )
+                    .position(x: geometry.size.width/2, y: geometry.size.height/2)
+            }
+            .frame(maxWidth: 375, maxHeight: 100)
+            .padding(.horizontal, 16)
+            .offset(x: viewState.width/17, y: viewState.height/17)
+            
+            Text("80 hours of courses for SwiftUI, React and design tools.")
+                .font(.subheadline)
+                .frame(width: 250)
+                .offset(x: viewState.width/20, y: viewState.height/20)
+            
+            
+            Spacer()
+        }
+        .multilineTextAlignment(.center)
+        .padding(.top, 100)
+        .frame(height: 477)
+        .frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                    .offset(x: -150, y: -200)
+                    .rotationEffect(Angle(degrees: show ? 360 + 90 : 90))
+                    .blendMode(.plusDarker)
+                    //                    .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
+                    .animation(nil)
+                    .onAppear { self.show = true }
+                
+                Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                    .offset(x: -200, y: -250)
+                    .rotationEffect(Angle(degrees: show ? 360 : 0 ), anchor: .leading)
+                    .blendMode(.overlay)
+                    //                     .animation(Animation.linear(duration: 100).repeatForever(autoreverses: false))
+                    .animation(nil)
+            }
+        )
+        .background(
+            Image(uiImage: #imageLiteral(resourceName: "Card3"))
+                .offset(x: viewState.width/25, y: viewState.height/25)
+            , alignment: .bottom)
+        .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .scaleEffect(isDragging ? 0.9 : 1)
+        .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+        .rotation3DEffect(
+            Angle(degrees: 5),
+            axis: (x: viewState.width, y: viewState.height, z: 0)
+        )
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    self.viewState = value.translation
+                    self.isDragging = true
+                }
+                .onEnded { value in
+                    self.viewState = .zero
+                    self.isDragging = false
+                }
+        )
     }
 }
