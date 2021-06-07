6ddc2201495a5b7e40668791d81891895ff2742e
Alex <alxsar98@gmail.com>
2021-06-07 16:48:24 +0300

    feat: connect firebase

--- DesignCode/DesignCodeApp.swift
+++ DesignCode/DesignCodeApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Firebase 
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("Colors application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct DesignCodeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             LoginView()
